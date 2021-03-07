a8803fa96828218576479c5736e5c39e7f0269ff
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-07 16:00:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const piblaster = require('pi-blaster.js');
 const converter = require('color-convert');
 const fs = require('fs');
 
-module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
 
-	homebridge.registerAccessory('homebridge-gpio-rgb-ledstrip', 'SmartLedStrip', SmartLedStripAccessory);
+  homebridge.registerAccessory('homebridge-gpio-rgb-ledstrip', 'SmartLedStrip', SmartLedStripAccessory);
 }
 
 function SmartLedStripAccessory(log, config) {
-  this.log      = log;
-  this.name     = config['name'];
+  this.log = log;
+  this.name = config['name'];
 
-  this.rPin     = config['rPin'];
-  this.gPin     = config['gPin'];
-  this.bPin     = config['bPin'];
+  this.rPin = config['rPin'];
+  this.gPin = config['gPin'];
+  this.bPin = config['bPin'];
 
-  this.enabled = true ;
+  this.enabled = true;
 
   try {
     if (!this.rPin)
@@ -42,33 +42,33 @@ function SmartLedStripAccessory(log, config) {
 
 SmartLedStripAccessory.prototype = {
 
-  getServices : function(){
+  getServices: function () {
 
-    if(this.enabled){
+    if (this.enabled) {
       let informationService = new Service.AccessoryInformation();
 
       informationService
-      .setCharacteristic(Characteristic.Manufacturer, 'Manfredi Pistone')
-      .setCharacteristic(Characteristic.Model, 'GPIO-RGB-LedStrip')
-      .setCharacteristic(Characteristic.SerialNumber, '06-06-00');
+        .setCharacteristic(Characteristic.Manufacturer, 'Tim')
+        .setCharacteristic(Characteristic.Model, 'GPIO-RGB-LedStrip')
+        .setCharacteristic(Characteristic.SerialNumber, '06-06-00');
 
       let smartLedStripService = new Service.Lightbulb(this.name);
 
       smartLedStripService
-          .getCharacteristic(Characteristic.On)
-          .on('change',this.toggleState.bind(this));
+        .getCharacteristic(Characteristic.On)
+        .on('change', this.toggleState.bind(this));
 
       smartLedStripService
-          .addCharacteristic(new Characteristic.Brightness())
-          .on('change',this.toggleState.bind(this));
+        .addCharacteristic(new Characteristic.Brightness())
+        .on('change', this.toggleState.bind(this));
 
       smartLedStripService
-          .addCharacteristic(new Characteristic.Hue())
-          .on('change',this.toggleState.bind(this));
+        .addCharacteristic(new Characteristic.Hue())
+        .on('change', this.toggleState.bind(this));
 
       smartLedStripService
-          .addCharacteristic(new Characteristic.Saturation())
-          .on('change',this.toggleState.bind(this));
+        .addCharacteristic(new Characteristic.Saturation())
+        .on('change', this.toggleState.bind(this));
 
       this.informationService = informationService;
       this.smartLedStripService = smartLedStripService;
@@ -76,54 +76,51 @@ SmartLedStripAccessory.prototype = {
       this.log("SmartLedStrip has been successfully initialized!");
 
       return [informationService, smartLedStripService];
-    }else{
+    } else {
       this.log("SmartLedStrip has not been initialized, please check your logs..");
       return [];
     }
 
   },
 
-  isOn : function() {
-      return this.smartLedStripService.getCharacteristic(Characteristic.On).value;
+  isOn: function () {
+    return this.smartLedStripService.getCharacteristic(Characteristic.On).value;
   },
 
-  getBrightness : function(){
+  getBrightness: function () {
     return this.smartLedStripService.getCharacteristic(Characteristic.Brightness).value;
   },
 
-  getHue : function(){
+  getHue: function () {
     return this.smartLedStripService.getCharacteristic(Characteristic.Hue).value;
   },
 
-  getSaturation : function(){
+  getSaturation: function () {
     return this.smartLedStripService.getCharacteristic(Characteristic.Saturation).value;
   },
 
-  toggleState : function()
-  {
-    if(this.enabled){
-      if(!this.isOn())
-      {
-          this.updateRGB(0,0,0);
-          return;
+  toggleState: function () {
+    if (this.enabled) {
+      if (!this.isOn()) {
+        this.updateRGB(0, 0, 0);
+        return;
       }
 
       var brightness = this.getBrightness();
-      if(brightness!=0){
-          var rgb = converter.hsv.rgb([this.getHue(), this.getSaturation(), brightness]);
-          this.updateRGB(rgb[0], rgb[1], rgb[2]);
-      }else{
-          this.updateRGB(0,0,0);
+      if (brightness != 0) {
+        var rgb = converter.hsv.rgb([this.getHue(), this.getSaturation(), brightness]);
+        this.updateRGB(rgb[0], rgb[1], rgb[2]);
+      } else {
+        this.updateRGB(0, 0, 0);
       }
     }
   },
 
-  updateRGB : function(red, green, blue)
-  {
-      this.log("Setting rgb values to: Red: "+red + " Green: "+green+ " Blue: "+blue);
-      piblaster.setPwm(this.rPin, red/255);
-      piblaster.setPwm(this.gPin, green/255);
-      piblaster.setPwm(this.bPin, blue/255);
+  updateRGB: function (red, green, blue) {
+    this.log("Setting rgb values to: Red: " + red + " Green: " + green + " Blue: " + blue);
+    piblaster.setPwm(this.rPin, red / 255);
+    piblaster.setPwm(this.gPin, green / 255);
+    piblaster.setPwm(this.bPin, blue / 255);
   }
 
 }
