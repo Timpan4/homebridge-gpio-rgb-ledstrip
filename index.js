ddea20cfbbd7cfa3b26da2d3778cabb16f6e4020
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-11 02:01:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ SmartLedStripAccessory.prototype = {
   updateRGB: function (red, green, blue, rPin, gPin, bPin) {
     let log = this;
     this.log("Trying to send request");
-    axios.post('http://khaosmc.asuscomm.com:3000/update', {
+    axios.post('/update', {
       rPin: rPin,
       gPin: gPin,
       bPin: bPin,
